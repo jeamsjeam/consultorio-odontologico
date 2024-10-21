0f3b9c1b75e62f389f66d37f5034ad2e7b3889b3
veruzkaksr <veruzkaksr22@gmail.com>
2024-10-20 20:02:21 -0400

    caracteristica: mejoras en el backend

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 #postgresql://postgres:admin@localhost:5432/ipasme
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@localhost:3306/ipasprueba"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@localhost:3306/consultorio"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # create the application database instance
